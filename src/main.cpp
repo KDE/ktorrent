29211836a47ff786c7972d80ea5ba201b253dc5e
Joris Guisson <joris.guisson@gmail.com>
2005-06-17 13:46:15 +0000

    Upped version to 1.0rc1
    svn path=/trunk/extragear/network/ktorrent/; revision=426503

--- src/main.cpp
+++ src/main.cpp
@@ -36,7 +36,7 @@
 static const char description[] =
     I18N_NOOP("A BitTorrent program for KDE");
 
-static const char version[] = "1.0rc1";
+static const char version[] = "1.0rc2";
 
 static KCmdLineOptions options[] =
 {
