18aeeff340f701a555be7756ea8e771148263b5a
Markus Brueffer <markus@brueffer.de>
2006-05-31 22:48:40 +0000

    FreeBSD needs netinet/in_systm.h for n_long
    svn path=/trunk/extragear/network/ktorrent/; revision=547141

--- libktorrent/mse/streamsocket.cpp
+++ libktorrent/mse/streamsocket.cpp
@@ -29,6 +29,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <netinet/in_systm.h>
 #include <netinet/ip.h> 
 #include <netinet/tcp.h>
 #include "streamsocket.h"
