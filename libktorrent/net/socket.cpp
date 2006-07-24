b516b3f824a248dcd5a74a87d27fda1a807ab0be
Joris Guisson <joris.guisson@gmail.com>
2006-07-24 16:26:55 +0000

    Made sure KT compiles on Mac OS X (fink)
    BUG: 131289
    
    
    svn path=/trunk/extragear/network/ktorrent/; revision=565838

--- libktorrent/net/socket.cpp
+++ libktorrent/net/socket.cpp
@@ -32,6 +32,9 @@
 #ifdef Q_OS_LINUX
 #include <asm/ioctls.h>
 #endif
+#ifdef Q_OS_MACX
+#define MSG_NOSIGNAL 0
+#endif
 #include <unistd.h>
 #include <fcntl.h>
 
@@ -46,6 +49,12 @@ namespace net
 
 	Socket::Socket(int fd) : m_fd(fd),m_state(IDLE)
 	{
+#ifdef Q_OS_MACX
+		if (setsockopt(m_fd,SOL_SOCKET,SO_NOSIGPIPE,0L,0) < 0)
+		{
+			Out(SYS_CON|LOG_NOTICE) << QString("Failed to set the NOSIGPIPE option : %1").arg(strerror(errno)) << endl;
+		}
+#endif
 	}
 	
 	Socket::Socket(bool tcp) : m_fd(-1),m_state(IDLE)
@@ -56,7 +65,12 @@ namespace net
 			Out(SYS_GEN|LOG_IMPORTANT) << QString("Cannot create socket : %1").arg(strerror(errno)) << endl;
 		}
 		m_fd = fd;
-		
+#ifdef Q_OS_MACX
+		if (setsockopt(m_fd,SOL_SOCKET,SO_NOSIGPIPE,0L,0) < 0)
+		{
+			Out(SYS_CON|LOG_NOTICE) << QString("Failed to set the NOSIGPIPE option : %1").arg(strerror(errno)) << endl;
+		}
+#endif	
 	}
 	
 	Socket::~Socket()
