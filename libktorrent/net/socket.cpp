499e232f0f2028b610366d2b48d4254350489862
Joris Guisson <joris.guisson@gmail.com>
2006-07-25 17:11:37 +0000

    Should now build on OSX or Darwin or whatever Qt calls it
    svn path=/trunk/extragear/network/ktorrent/; revision=566272

--- libktorrent/net/socket.cpp
+++ libktorrent/net/socket.cpp
@@ -32,7 +32,7 @@
 #ifdef Q_OS_LINUX
 #include <asm/ioctls.h>
 #endif
-#ifdef Q_OS_MACX
+#if defined(Q_OS_MACX) || defined(Q_OS_DARWIN)
 #define MSG_NOSIGNAL 0
 #endif
 #include <unistd.h>
@@ -49,7 +49,7 @@ namespace net
 
 	Socket::Socket(int fd) : m_fd(fd),m_state(IDLE)
 	{
-#ifdef Q_OS_MACX
+#if defined(Q_OS_MACX) || defined(Q_OS_DARWIN)
 		if (setsockopt(m_fd,SOL_SOCKET,SO_NOSIGPIPE,0L,0) < 0)
 		{
 			Out(SYS_CON|LOG_NOTICE) << QString("Failed to set the NOSIGPIPE option : %1").arg(strerror(errno)) << endl;
@@ -65,7 +65,7 @@ namespace net
 			Out(SYS_GEN|LOG_IMPORTANT) << QString("Cannot create socket : %1").arg(strerror(errno)) << endl;
 		}
 		m_fd = fd;
-#ifdef Q_OS_MACX
+ #if defined(Q_OS_MACX) || defined(Q_OS_DARWIN)
 		if (setsockopt(m_fd,SOL_SOCKET,SO_NOSIGPIPE,0L,0) < 0)
 		{
 			Out(SYS_CON|LOG_NOTICE) << QString("Failed to set the NOSIGPIPE option : %1").arg(strerror(errno)) << endl;
