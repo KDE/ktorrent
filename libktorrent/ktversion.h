d04664f483ca773336e544aa9f6b2e42f7a353a4
Joris Guisson <joris.guisson@gmail.com>
2006-08-20 11:36:55 +0000

    Bumped version number of 2.0 branch to 2.0.1
    svn path=/branches/ktorrent/2.0/; revision=574837

--- libktorrent/ktversion.h
+++ libktorrent/ktversion.h
@@ -27,8 +27,9 @@ namespace kt
 {
 	const bt::Uint32 MAJOR = 2;
 	const bt::Uint32 MINOR = 0;
-	const char VERSION_STRING[] = "2.0";
-	const char PEER_ID[] = "-KT2000-";
+	const bt::Uint32 RELEASE = 1;
+	const char VERSION_STRING[] = "2.0.1";
+	const char PEER_ID[] = "-KT2010-";
 }
 
 
