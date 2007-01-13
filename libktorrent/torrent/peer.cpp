05bdeb7aefe1ee025385c21c359686a8fc6bf4c3
Joris Guisson <joris.guisson@gmail.com>
2007-01-13 19:26:13 +0000

    Only consider a peer snubbed when the snubbed timer has expired AND there are no pending download requests.
    CCMAIL: changelog@ktorrent.org
    
    
    svn path=/trunk/extragear/network/ktorrent/; revision=623020

--- libktorrent/torrent/peer.cpp
+++ libktorrent/torrent/peer.cpp
@@ -465,7 +465,7 @@ namespace bt
 	bool Peer::isSnubbed() const
 	{
 		// 4 minutes
-		return snub_timer.getElapsedSinceUpdate() >= 2*60*1000;
+		return snub_timer.getElapsedSinceUpdate() >= 2*60*1000 && stats.num_down_requests > 0;
 	}
 
 	bool Peer::isSeeder() const
