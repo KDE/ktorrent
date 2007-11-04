b936a57f89ced2dd1cc8cbbff1e28c174af31245
Joris Guisson <joris.guisson@gmail.com>
2007-11-04 12:40:29 +0000

    Ported patch from Rafael to KDE4
    svn path=/trunk/extragear/network/ktorrent/; revision=732651

--- plugins/webinterface/www/default/details.php
+++ plugins/webinterface/www/default/details.php
@@ -1,5 +1,5 @@
 <?php
-	$stats=downloadStatus(); 
+	$stats=downloadStatus();
 	$num_torrent=$_REQUEST['torrent'];
 	if(strlen($stats[$num_torrent]['torrent_name'])>30)
 		$display_name=substr($stats[$num_torrent]['torrent_name'], 0, 30)."...";
@@ -9,84 +9,59 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-  <head>
-    <style type="text/css" media="all">@import "stylen.css";
-
-	.settingsInput
+<head>
+<style type="text/css" media="all">
+	@import "stylen.css";
+</style>
+<meta http-equiv="Content-Type" content="text/html" />
+<link rel="icon" href="favicon.ico" type="image/x-icon" />
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+<title><?php echo 'KTorrent: Details for '.$display_name; ?></title>
+<script type="text/javascript">
+	function validate()
 	{
-		width:50px;
+		msg = "Are you absolutely sure that you want remove this torrent?";
+		return confirm(msg);
 	}
-
-    </style>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <link rel="icon" href="favicon.ico" type="image/x-icon"/>
-    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>
-    <title><?php echo 'KTorrent: Details for '.$display_name; ?></title>
-
-  </head>
-  <body>
-    <div id="page-container">
-      <div id="top_bar">
-        WebInterface KTorrent plugin
-      </div>
-      <div id="icon">
-        <img src="icon.png" alt="KT Icon"/>
-      </div>
-      <div id="header">
-        <div id="head-content">
-          <strong>KTorrent WebInterface</strong>
-          <br />
-          <font size="-1">BitTorrent client for KDE</font>
-        </div>
-		<div id="menu">
-				<div style="padding-left:130px;">
-
-					<dl>
-					<?php
-							echo "<dt><a href=\"details.php?torrent=$num_torrent\" title=\"REFRESH\">Refresh</a></dt>";
-					?>
-							<dt><a href="interface.php" title="BACK">Back</a></dt>
-							<dt><a href="login.html" title="LOGOUT">Logout</a></dt>
-					</dl>
-				</div>
-			</div>
-      </div>
-      <div id="content">
-        <table>
-          <tr>
-            <td align="left" style="padding:2px;">
-              <strong>Actions</strong>
-            </td>
-            <td align="left" style="padding:2px;">
-              <strong>File</strong>
-            </td>
-            <td align="left" style="padding:2px;">
-              <strong>Status</strong>
-            </td>
-            <td align="right" style="padding:2px;">
-              <strong>Size</strong>
-            </td>
-            <td align="left" style="padding:2px;">
-              <strong>Complete</strong>
-            </td>
-          </tr>
-
-	  <?php
+</script>
+</head>
+<body>
+	<div id="top_bar">WebInterface KTorrent plugin</div>
+	<div id="icon"><img src="icon.png" alt="" /></div>
+	<div id="header">
+		<strong>KTorrent WebInterface</strong>
+		<br />
+        	<small>BitTorrent client for KDE</small>
+	</div>
+	<ul id="menu">
+		<li><a href="interface.php" title="BACK">Back</a></li>
+		<li><a href="login.html" title="LOGOUT">Logout</a></li>
+	</ul>
+	<div id="content">
+		<table>
+		<tr>
+			<th>Actions</th>
+			<th>File</th>
+			<th>Status</th>
+			<th>Size</th>
+			<th>Complete</th>
+		</tr>
+		<?php
 		$files_to_display=$stats[$num_torrent]['num_files'];
 		$display_files = $stats[$num_torrent]['files'];
 
 		for ($i = 0; $i < $files_to_display; $i++)
 		{
-			$file_pos="file_$i";
-			$size_pos="size_$i";
-			$perc_pos="perc_done_$i";
-			$status_pos="status_$i";
+			$file_pos='file_'.$i;
+			$size_pos='size_'.$i;
+			$perc_pos='perc_done_'.$i;
+			$status_pos='status_'.$i;
 
-			echo "<tr>";
-			echo "<td><a href=\"details.php?file_hp=$num_torrent-$i&amp;torrent=$num_torrent\" title=\"High Priority\"><img src=\"/high_priority.png\" name=\"stop\" width=\"16\" height=\"16\" border=\"0\" style=\"padding:2px;\" alt=\"/\\\" /></a>";
-			echo "<a href=\"details.php?file_np=$num_torrent-$i&amp;torrent=$num_torrent\" title=\"Normal Priority\"><img src=\"/normal_priority.png\" name=\"start\" width=\"16\" height=\"16\" border=\"0\" style=\"padding:2px;\" alt=\"-\" /></a>";
-			echo "<a href=\"details.php?file_lp=$num_torrent-$i&amp;torrent=$num_torrent\" title=\"Low Priority\"><img src=\"/low_priority.png\" name=\"start\" width=\"16\" height=\"16\" border=\"0\" style=\"padding:2px;\" alt=\"\\/\" /></a>";
-			echo "<a href=\"details.php?file_dnd=$num_torrent-$i&amp;torrent=$num_torrent\" title=\"Only Seed Priority\"><img src=\"/dnd.png\" name=\"remove\" width=\"16\" height=\"16\" border=\"0\" style=\"padding:2px;\" alt=\"X\" /></a></td>";
+			echo '<tr>';
+			echo "<td class=\"actions\"><a href=\"details.php?file_hp=$num_torrent-$i&amp;torrent=$num_torrent\" title=\"High Priority\"><img src=\"/high_priority.png\" name=\"stop\" alt=\"/\\\" /></a>";
+			echo "<a href=\"details.php?file_np=$num_torrent-$i&amp;torrent=$num_torrent\" title=\"Normal Priority\"><img src=\"/normal_priority.png\" name=\"start\" alt=\"-\" /></a>";
+			echo "<a href=\"details.php?file_lp=$num_torrent-$i&amp;torrent=$num_torrent\" title=\"Low Priority\"><img src=\"/low_priority.png\" name=\"start\" alt=\"\\/\" /></a>";
+			echo "<a href=\"details.php?file_dnd=$num_torrent-$i&amp;torrent=$num_torrent\" title=\"Only Seed Priority\"><img src=\"/dnd.png\" name=\"remove\" alt=\"X\" /></a></td>";
 
 			if(strlen($display_files[$file_pos])>30)
 				$file_display=substr($display_files[$file_pos], 0, 30)."...";
@@ -95,30 +70,16 @@
 					
 			$perc_display=round($display_files[$perc_pos], 2);
 
-			echo "<td>$file_display</td>";
-			echo "<td>$display_files[$status_pos]</td>";
-			echo "<td style=\"text-align:right;\">$display_files[$size_pos]</td>";
-			echo "<td style=\"text-align:right;\">$perc_display %</td>";
-			echo "</tr>";
+			echo '<td>'.$file_display.'</td>';
+			echo '<td>'.$display_files[$status_pos].'</td>';
+			echo '<td style="text-align:right;">'.$display_files[$size_pos].'</td>';
+			echo '<td style="text-align:right;">'.$perc_display.' %</td>';
+			echo '</tr>';
 		}
-	  ?>
-	 </table>
-	
+		?>
+		</table>
 	</div>
-      <div id="footer">
-        <table width="100%" align="center">
-          <tr>
-            <td>
-            </td>
-            <td>
-               &#169; 2006 WebInterface KTorrent plugin
-            </td>
-            <td>
-              <a href="login.html" title="LOGOUT">Logout</a> | <a href="shutdown.php?quit=quit" title="Shutdown KTorrent">Shutdown KTorrent</a>
-            </td>
-          </tr>
-        </table>
-      </div>
-     </div>
-  </body>
+	<div id="footer">&#169; 2006 WebInterface KTorrent plugin</div>
+	<script type="text/javascript" src="wz_tooltip.js"></script>
+</body>
 </html>
--- plugins/webinterface/www/default/interface.php
+++ plugins/webinterface/www/default/interface.php
@@ -1,321 +1,147 @@
 <?php
-	$globalinfo=globalInfo();
-	$stats=downloadStatus(); 
+$globalinfo=globalInfo();
+$stats=downloadStatus();
+
+function get_torrent_status_name($status_id)
+{
+	$table = array(
+	0 => 'Not Started',
+	1 => 'Seeding Complete',
+	2 => 'Download Complete',
+	3 => 'Seeding',
+	4 => 'Downloading',
+	5 => 'Stalled',
+	6 => 'Stopped',
+	7 => 'Allocating Diskspace',
+	8 => 'Error',
+	9 => 'Queued',
+	10 => 'Checking Data'
+	);
+	if (array_key_exists($status_id, $table)) return $table[$status_id];
+	else return 'Not supported Status';
+}
+
+function generate_button_code($img, $alt, $href='')
+{
+	$img = '<img src="'.htmlspecialchars($img).'" alt="'.htmlspecialchars($alt).'" />';
+	if (empty($href)) return $img;
+	else return '<a href="'.htmlspecialchars($href).'">'.$img.'</a>';
+}
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-  <head>
-    <style type="text/css" media="all">@import "stylen.css";
-
-	.settingsInput
-	{
-		width:50px;
-	}
-
-    </style>
-    <meta http-equiv="Content-Type" content="text/html"/>
-    <link rel="icon" href="favicon.ico" type="image/x-icon"/>
-    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>
-    <title><?php echo '(D:'.$globalinfo['download_speed'].') (U:'.$globalinfo['upload_speed'].') KTorrent'; ?></title>
-    <script type="text/javascript">
-
+<head>
+<style type="text/css" media="all">
+	@import "stylen.css";
+</style>
+<meta http-equiv="Content-Type" content="text/html" />
+<link rel="icon" href="favicon.ico" type="image/x-icon" />
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+<title><?php echo '(D:'.$globalinfo['download_speed'].') (U:'.$globalinfo['upload_speed'].') KTorrent'; ?></title>
+<script type="text/javascript">
 	function validate()
 	{
 		msg = "Are you absolutely sure that you want remove this torrent?";
 		return confirm(msg);
 	}
+</script>
+</head>
+<body>
+	<div id="top_bar">WebInterface KTorrent plugin</div>
+	<div id="icon"><img src="icon.png" alt="" /></div>
+	<div id="header">
+		<strong>KTorrent WebInterface</strong>
+		<br />
+        	<small>BitTorrent client for KDE</small>
+	</div>
+	<ul id="menu">
+		<li><a href="interface.php" title="REFRESH">Refresh</a></li>
+		<li><a href="shutdown.php?quit=quit" title="Shutdown KTorrent">Shutdown</a></li>
+		<li><a href="login.html" title="LOGOUT">Logout</a></li>
+	</ul>
+	<div id="sidebar">
+		<div class="box">
+			<h2>Torrent control</h2>
+			<form action="interface.php" method="get" style="text-align: center;">
+			<input type="submit" name="startall" value="Start All" />
+			</form>
+			<hr />
+			<form action="interface.php" method="get" style="text-align: center;">
+			<input type="submit" name="stopall" value="Stop All" />
+			</form>
+		</div>
+		<div class="box">
+			<h2>Settings</h2>
+			<form action="interface.php" method="get">
+				<label>Upload speed <input type="text" name="maximum_upload_rate" value="<?php echo $globalinfo['max_upload_speed']; ?>" class="settingsInput" /></label>
+				<label>Download speed <input type="text" name="maximum_download_rate" value="<?php echo $globalinfo['max_download_speed']; ?>" class="settingsInput" /></label>
+				<label>Maximum downloads <input type="text" name="maximum_downloads" value="<?php echo $globalinfo['max_downloads']; ?>" class="settingsInput" /></label>
+				<label>Maximum seeds <input type="text" name="maximum_seeds" value="<?php echo $globalinfo['max_seeds']; ?>" class="settingsInput" /></label>
+                        	<input type="submit" value="Submit settings" />
+			</form>
+		</div>
+		<div class="box">
+			<h2>Load torrents</h2>
+			<form action="interface.php" method="get">
+			<label class="wide">Torrent URL: <input type="text" name="load_torrent" /></label>
+			<input type="submit" value="Load Torrent" />
+			</form>
+			<hr />
+			<form method="post" enctype="multipart/form-data" action="interface.php">
+			<label class="wide">Local File:<input type="file" name="load_torrent" /></label>
+			<input type="submit" name="Upload Torrent" value="Upload Torrent" />
+			</form>
+		</div>
+	</div>
+	<div id="content">
+		<table>
+		<tr>
+			<th>Actions</th>
+			<th>File</th>
+			<th>Status</th>
+			<th>Downloaded</th>
+			<th>Size</th>
+			<th>Uploaded</th>
+			<th>Down Speed</th>
+			<th>Up Speed</th>
+			<th>Peers</th>
+			<th>Complete</th>
+		</tr>
+<?php
+		$a = 0;
+		foreach ($stats as $torrent) {
+			echo "\t\t".'<tr>'."\n\t\t\t";
 
-	</script>
-  </head>
-  <body>
-    <div id="page-container">
-      <div id="top_bar">
-        WebInterface KTorrent plugin
-      </div>
-      <div id="icon">
-        <img src="icon.png" alt="KT Icon"/>
-      </div>
-      <div id="header">
-        <div id="head-content">
-          <strong>KTorrent WebInterface</strong>
-          <br />
-          <font size="-1">BitTorrent client for KDE</font>
-        </div>
-		<div id="menu">
-				<div style="padding-left:130px;">
+			$torrent_name = str_replace("'", "\'", $torrent['torrent_name']);
+			if($torrent['total_bytes_to_download']!=0) $perc = round(100.0 - ($torrent['bytes_left_to_download'] / $torrent['total_bytes_to_download']) * 100.0, 2);
+			else $perc = 0;
+			if(strlen($torrent['torrent_name'])>30) $display_name=substr($torrent['torrent_name'], 0, 30)." ...";
+			else $display_name=$torrent['torrent_name'];
+			if ($torrent['num_files']>1) $file_td_content = '<a href="details.php?torrent='.$a.'">'.$display_name.'</a>';
+			else $file_td_content = $display_name;
 
-					<dl>
-							<dt><a href="interface.php" title="REFRESH">Refresh</a></dt>
-							<dt><a href="login.html" title="LOGOUT">Logout</a></dt>
-					</dl>
-				</div>
-			</div>
-      </div>
-      <div id="sidebar">
-        <div id="sidebar-content">
-			<!-- Control -->
-			<div align="center" style="padding-bottom: 10px;">
-			<table class="box" align="center" cellspacing="0" cellpadding="2">
-                          <tr>
-                            <td class="box-header">
-                               Torrent control
-                            </td>
-                          </tr>
-                          <tr align="center">
-                            <td>
-                              <form action="interface.php" method="get">
-                                <input type="submit" name="startall" value="Start All" class="buttons" />
-                              </form>
-                              <form action="interface.php" method="get">
-                                <input type="submit" name="stopall" value="Stop All" class="buttons" />
-                              </form>
-                            </td>
-                          </tr>
-                        </table>
-			</div>
-          <!-- SETTINGS  -->
-          <div align="center" style="padding-bottom: 10px;">
-            <table cellspacing="0" cellpadding="2" class="box" style="width:250px;">
-              <tbody>
-                <tr>
-                  <td class="box-header">
-                     Settings
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <form action="interface.php" method="get">
-                      <table class="box-settings" style="text-align:left;">
-                        <tr>
-                          <td>
-                             Upload speed
-                          </td>
-                          <td>
-                            <input type="text" name="maximum_upload_rate" value="<?php echo $globalinfo['max_upload_speed']; ?>" class="settingsInput" />
-                          </td>
-                        </tr>
-                        <tr>
-                          <td>
-                             Download speed
-                          </td>
-                          <td>
-                            <input type="text" name="maximum_download_rate" value="<?php echo $globalinfo['max_download_speed']; ?>" class="settingsInput" />
-                          </td>
-                        </tr>
-                        <tr>
-                          <td>
-                             Maximum downloads
-                          </td>
-                          <td>
-                            <input type="text" name="maximum_downloads" value="<?php echo $globalinfo['max_downloads']; ?>" class="settingsInput" />
-                          </td>
-                        </tr>
-                        <tr>
-                          <td>
-                             MaximumSeeds
-                          </td>
-                          <td>
-                            <input type="text" name="maximum_seeds" value="<?php echo $globalinfo['max_seeds']; ?>" class="settingsInput" />
-                          </td>
-                        </tr>
-                        <tr>
-                          <td>
-                          </td>
-                          <td>
-                          </td>
-                        </tr>
-                        <tr>
-                          <td colspan="2" style="text-align:right;">
-                            <input type="submit" value="Submit settings" class="buttons"/>
-                          </td>
-                        </tr>
-                      </table>
-                    </form>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-          <!-- Torrent -->
-          <div align="center" style="padding-bottom: 10px;">
-                        <table cellspacing="0" cellpadding="2" class="box">
-                          <tbody>
-                            <tr>
-                              <td class="box-header">
-                                 Load Torrents
-                              </td>
-                            </tr>
-                            <form action="interface.php" method="get">
-                              <tr>
-                                <td>
-                                   Torrent URL: <input type="text" name="load_torrent" style="width:240px;" />
-                                </td>
-                              </tr>
-                              <tr>
-                                <td style="text-align:right;">
-                                  <input type="submit" value="Load Torrent" class="buttons" />
-                                </td>
-                              </tr>
-                            </form>
-                            <tr>
-                              <td>
-                                <hr/>
-                              </td>
-                            </tr>
-                            <form method="post" enctype="multipart/form-data" action="interface.php">
-                              <tr>
-                                <td>
-                                   Local File:<input type="file" name="load_torrent" style="width:240px;" />
-                                </td>
-                              </tr>
-                              <tr>
-                                <td style="text-align:right;">
-                                  <input type="submit" name="Upload Torrent" value="Upload Torrent" class="buttons" />
-                                </td>
-                              </tr>
-                            </form>
-                            <tr>
-                              <td>
-                                <hr/>
-                              </td>
-                            </tr>
-                            <tr align="center">
-                              <td>
-                              </td>
-                            </tr>
-                          </tbody>
-                        </table>
-                      </div>
-      
-</div>
-
-   </div>
-      <div id="content">
-        <table>
-          <tr>
-            <td align="left" style="padding:2px;">
-              <strong>Actions</strong>
-            </td>
-            <td align="left" style="padding:2px;">
-              <strong>File</strong>
-            </td>
-            <td align="left" style="padding:2px;">
-              <strong>Status</strong>
-            </td>
-            <td align="left" style="padding:2px;">
-              <strong>Downloaded</strong>
-            </td>
-            <td align="left" style="padding:2px;">
-              <strong>Size</strong>
-            </td>
-            <td align="left" style="padding:2px;">
-              <strong>Uploaded</strong>
-            </td>
-            <td align="left" style="padding:2px;">
-              <strong>Down Speed</strong>
-            </td>
-            <td align="left" style="padding:2px;">
-              <strong>Up Speed</strong>
-            </td>
-            <td align="left" style="padding:2px;">
-              <strong>Peers</strong>
-            </td>
-            <td align="left" style="padding:2px;">
-              <strong>Complete</strong>
-            </td>
-          </tr>
-          <?php
-				$stats=downloadStatus();
-				$a = 0;
-				foreach ($stats as $torrent) {
-					echo "<tr>";
-					
-					$torrent_name = str_replace("'", "\'", $torrent['torrent_name']);
-					
-					if($torrent['total_bytes_to_download']!=0)
-						$perc = round(100.0 - ($torrent['bytes_left_to_download'] / $torrent['total_bytes_to_download']) * 100.0, 2);
-					else
-						$perc = 0;
-					echo "<td><a href=\"interface.php?stop=$a\" title=\"STOP\"><img src=\"/stop.png\" name=\"stop\" width=\"16\" height=\"16\" border=\"0\" style=\"padding:2px;\" alt=\"\" /></a>";
-					echo "<a href=\"interface.php?start=$a\" title=\"START\"><img src=\"/start.png\" name=\"start\" width=\"16\" height=\"16\" border=\"0\" style=\"padding:2px;\" alt=\"\" /></a>";
-					echo "<a href=\"interface.php?remove=$a\" title=\"REMOVE\" onclick=\"return validate()\"><img src=\"/remove.png\" name=\"remove\" width=\"16\" height=\"16\" border=\"0\" style=\"padding:2px;\" alt=\"\" /></a></td>";
-
-					if(strlen($torrent['torrent_name'])>30)
-						$display_name=substr($torrent['torrent_name'], 0, 30)." ...";
-					else
-						$display_name=$torrent['torrent_name'];
-					
-					if ($torrent['num_files']>1)
-						echo "<td style=\"text-align:left;\" onmouseover=\"this.T_TITLE='$torrent_name';return escape('Download speed:&lt;strong&gt;{$torrent['download_rate']}&lt;/strong&gt;&lt;br /&gt; Upload speed:&lt;strong&gt;{$torrent['upload_rate']}&lt;/strong&gt;&lt;/td&gt;')\"><a href=\"details.php?torrent=$a\">$display_name</a></td>";
-					else
-						echo "<td style=\"text-align:left;\" onmouseover=\"this.T_TITLE='$torrent_name';return escape('Download speed:&lt;strong&gt;{$torrent['download_rate']}&lt;/strong&gt;&lt;br /&gt; Upload speed:&lt;strong&gt;{$torrent['upload_rate']}&lt;/strong&gt;&lt;/td&gt;')\">$display_name</td>";
-
-					switch ($torrent['status']) {
-						case 0:
-   							echo "<td>Not Started</td>";
-							break;
-						case 1:
-  							echo "<td>Seeding Complete</td>";
-   							break;
-						case 2:
-							echo "<td>Download Complete</td>";
-							break;
-						case 3:
-							echo "<td>Seeding</td>";
-							break;
-						case 4:
-							echo "<td>Downloading</td>";
-							break;
-						case 5:
-							echo "<td>Stalled</td>";
-							break;
-						case 6:
-							echo "<td>Stopped</td>";
-							break;
-						case 7:
-							echo "<td>Allocating Diskspace</td>";
-							break;
-						case 8:
-							echo "<td>Error</td>";
-							break;
-						case 9:
-							echo "<td>Queued</td>";
-							break;
-						case 10:
-							echo "<td>Checking Data</td>";
-							break;
-						default:
-							echo "<td>Not supported Status</td>";
-					}
-					echo "<td style=\"text-align:right;\">{$torrent['bytes_downloaded']}</td>";
-					echo "<td style=\"text-align:right; padding-left:8px;\">{$torrent['total_bytes']}</td>";
-					echo "<td style=\"text-align:right; padding-left:8px;\">{$torrent['bytes_uploaded']}</td>";
-					echo "<td style=\"text-align:right;\">{$torrent['download_rate']}</td>";
-					echo "<td style=\"text-align:right;\">{$torrent['upload_rate']}</td>";
-					echo "<td>{$torrent['num_peers']}</td>";
-					echo "<td style=\"text-align:right;\">$perc %</td>";
-					echo "</tr>";
-				$a++;
-				}
-          ?>
-        </table>
-      </div>
-      <div id="footer">
-        <table width="100%" align="center">
-          <tr>
-            <td>
-            </td>
-            <td>
-               &#169; 2006 WebInterface KTorrent plugin
-            </td>
-            <td>
-              <a href="login.html" title="LOGOUT">Logout</a> |  <a href="shutdown.php?quit=quit" title="Shutdown KTorrent">Shutdown KTorrent</a>
-            </td>
-          </tr>
-        </table>
-      </div>
-    </div>
-    <script type="text/javascript" src="wz_tooltip.js"></script>
-  </body>
+			echo '<td class="actions">';
+			echo generate_button_code('/stop.png', 'stop', ($torrent['running'])?'interface.php?stop='.$a:'');
+			echo generate_button_code('/start.png', 'start', ($torrent['running'])?'':'interface.php?start='.$a);
+			echo '<a href="interface.php?remove='.$a.'" onclick="return validate()"><img src="/remove.png" alt="remove" /></a>';
+			echo '</td>';
+			echo "<td style=\"text-align:left;\" onmouseover=\"this.T_TITLE='$torrent_name';return escape('Download speed:&lt;strong&gt;{$torrent['download_rate']}&lt;/strong&gt;&lt;br /&gt; Upload speed:&lt;strong&gt;{$torrent['upload_rate']}&lt;/strong&gt;&lt;/td&gt;')\">$file_td_content</td>";
+			echo '<td>'.get_torrent_status_name($torrent['status']).'</td>';
+			echo '<td style="text-align:right;">'.$torrent['bytes_downloaded'].'</td>';
+			echo '<td style="text-align:right; padding-left:8px;">'.$torrent['total_bytes'].'</td>';
+			echo '<td style="text-align:right; padding-left:8px;">'.$torrent['bytes_uploaded'].'</td>';
+			echo '<td style="text-align:right;">'.$torrent['download_rate'].'</td>';
+			echo '<td style="text-align:right;">'.$torrent['upload_rate'].'</td>';
+			echo '<td>'.$torrent['num_peers'].'</td>';
+			echo '<td style="text-align:right;">'.$perc.'% </td>';
+			echo "\n\t\t".'</tr>'."\n";
+		$a++;
+		}
+		?>
+		</table>
+	</div>
+	<div id="footer">&#169; 2006 WebInterface KTorrent plugin</div>
+	<script type="text/javascript" src="wz_tooltip.js"></script>
+</body>
 </html>
