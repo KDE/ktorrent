1c9d5158a8aa34df310c11d795106a6ebfe9b062
Joris Guisson <joris.guisson@gmail.com>
2007-12-18 12:35:28 +0000

    Updated version number to 3.0beta1
    svn path=/trunk/extragear/network/ktorrent/; revision=749984

--- libktcore/ktversion.h
+++ libktcore/ktversion.h
@@ -27,10 +27,10 @@ namespace kt
 {
 	const bt::Uint32 MAJOR = 3;
 	const bt::Uint32 MINOR = 0;
-	const char VERSION_STRING[] = "3.0dev";
-	const char PEER_ID[] = "-KT30DV-";
+	const char VERSION_STRING[] = "3.0beta1";
+	const char PEER_ID[] = "-KT30B1-";
 }
 
-#define KT_VERSION_MACRO "3.0dev"
+#define KT_VERSION_MACRO "3.0beta1"
 
 #endif
