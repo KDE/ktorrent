d86ae57ddeb34cddfcfdc0f9323ca92f8c62f669
Joris Guisson <joris.guisson@gmail.com>
2008-03-25 09:25:10 +0000

    - Merged rev 789797 (fix for seed time wrap around bug, 159756) - Upped version number to 3.0.2 in 3.0 branch
    svn path=/branches/ktorrent/3.0/; revision=789798

--- libktcore/ktversion.h
+++ libktcore/ktversion.h
@@ -27,10 +27,10 @@ namespace kt
 {
 	const bt::Uint32 MAJOR = 3;
 	const bt::Uint32 MINOR = 0;
-	const bt::Uint32 RELEASE = 1;
-	const char VERSION_STRING[] = "3.0.1";
+	const bt::Uint32 RELEASE = 2;
+	const char VERSION_STRING[] = "3.0.2";
 }
 
-#define KT_VERSION_MACRO "3.0.1"
+#define KT_VERSION_MACRO "3.0.2"
 
 #endif
