7fee10a846452dd4f57bb26d7ad05acbc558a914
Joris Guisson <joris.guisson@gmail.com>
2008-04-28 10:36:18 +0000

    Created 3.1beta1 tag of ktorrent
    svn path=/tags/ktorrent/3.1beta1/; revision=802022

--- ktorrent/main.cpp
+++ ktorrent/main.cpp
@@ -70,7 +70,7 @@ int main(int argc, char **argv)
 	signal(SIGPIPE,SIG_IGN);
 	signal(SIGXFSZ,SIG_IGN);
 	
-	bt::SetClientInfo("KTorrent",kt::MAJOR,kt::MINOR,kt::RELEASE,bt::DEVEL,"KT");
+	bt::SetClientInfo("KTorrent",kt::MAJOR,kt::MINOR,kt::RELEASE,bt::BETA,"KT");
 	
 	KAboutData about(
 		"ktorrent", 0, ki18n("KTorrent"),
--- libktcore/ktversion.h
+++ libktcore/ktversion.h
@@ -27,10 +27,10 @@ namespace kt
 {
 	const bt::Uint32 MAJOR = 3;
 	const bt::Uint32 MINOR = 1;
-	const bt::Uint32 RELEASE = 0;
-	const char VERSION_STRING[] = "3.1dev";
+	const bt::Uint32 RELEASE = 1;
+	const char VERSION_STRING[] = "3.1beta1";
 }
 
-#define KT_VERSION_MACRO "3.1dev"
+#define KT_VERSION_MACRO "3.1beta1"
 
 #endif
