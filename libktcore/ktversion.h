3154593e7b90069167e18439afd05a870e742219
Joris Guisson <joris.guisson@gmail.com>
2008-05-18 11:40:33 +0000

    Added 3.1beta2 tag
    svn path=/tags/ktorrent/3.1beta2/; revision=809106

--- libktcore/ktversion.h
+++ libktcore/ktversion.h
@@ -27,10 +27,10 @@ namespace kt
 {
 	const bt::Uint32 MAJOR = 3;
 	const bt::Uint32 MINOR = 1;
-	const bt::Uint32 RELEASE = 0;
-	const char VERSION_STRING[] = "3.1dev";
+	const bt::Uint32 RELEASE = 2;
+	const char VERSION_STRING[] = "3.1beta2";
 }
 
-#define KT_VERSION_MACRO "3.1dev"
+#define KT_VERSION_MACRO "3.1beta2"
 
 #endif
