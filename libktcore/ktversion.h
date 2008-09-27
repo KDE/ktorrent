147209b34a93311dc3bcea6838791e807f136eab
Joris Guisson <joris.guisson@gmail.com>
2008-09-27 10:37:10 +0000

    Upped version number to 3.1.3
    svn path=/branches/stable/extragear-kde4/network/ktorrent/; revision=865274

--- libktcore/ktversion.h
+++ libktcore/ktversion.h
@@ -27,10 +27,10 @@ namespace kt
 {
 	const bt::Uint32 MAJOR = 3;
 	const bt::Uint32 MINOR = 1;
-	const bt::Uint32 RELEASE = 2;
-	const char VERSION_STRING[] = "3.1.2";
+	const bt::Uint32 RELEASE = 3;
+	const char VERSION_STRING[] = "3.1.3";
 }
 
-#define KT_VERSION_MACRO "3.1.2"
+#define KT_VERSION_MACRO "3.1.3"
 
 #endif
