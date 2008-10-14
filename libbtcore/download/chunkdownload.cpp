3c26f647e32f68e7a9ad757f9d0eb6cd4d2b7c6c
Joris Guisson <joris.guisson@gmail.com>
2008-10-14 16:50:37 +0000

    Added some safety checks to fix a potential crash
    svn path=/branches/stable/extragear-kde4/network/ktorrent/; revision=871339

--- libbtcore/download/chunkdownload.cpp
+++ libbtcore/download/chunkdownload.cpp
@@ -107,6 +107,12 @@ namespace bt
 		if (pieces.get(pp))
 			return false;
 
+		if (!p.getData() || p.getOffset() + p.getLength() > chunk->getSize())
+		{
+			Out(SYS_CON|LOG_DEBUG) << "There is something wrong with a received piece !" << endl;
+			Out(SYS_CON|LOG_DEBUG) << p.getOffset() << " " << p.getLength() << " " << chunk->getSize() << endl;
+			return false;
+		}
 	
 		DownloadStatus* ds = dstatus.find(p.getPieceDownloader());
 		if (ds)
