fef9d9b7050bb0ee745d90cd384c7e1d95bd2209
Joris Guisson <joris.guisson@gmail.com>
2009-02-26 17:34:48 +0000

    Some fixes for windows
    svn path=/branches/stable/extragear-kde4/network/ktorrent/; revision=932457

--- libbtcore/net/wakeuppipe.cpp
+++ libbtcore/net/wakeuppipe.cpp
@@ -30,33 +30,41 @@ namespace net
 
 	WakeUpPipe::WakeUpPipe() : reader(-1),writer(-1)
 	{
+#ifndef Q_WS_WIN
 		int sockets[2];
 		if (socketpair(AF_UNIX,SOCK_STREAM,0,sockets) == 0)
 		{
 			reader = sockets[1];
 			writer = sockets[0];
 		}
+#endif
 	}
 
 
 	WakeUpPipe::~WakeUpPipe()
 	{
+#ifndef Q_WS_WIN
 		::close(reader);
 		::close(writer);
+#endif
 	}
 
 	void WakeUpPipe::wakeUp()
 	{
+#ifndef Q_WS_WIN
 		char dummy[] = "dummy";
 		int ret = write(writer,dummy,5);
 		if (ret != 5)
 			Out(SYS_GEN|LOG_DEBUG) << "WakeUpPipe: wake up failed " << ret << endl;
+#endif
 	}
 		
 	void WakeUpPipe::handleData()
 	{
+#ifndef Q_WS_WIN
 		bt::Uint8 buf[20];
 		if (read(reader,buf,20) < 0)
 			Out(SYS_GEN|LOG_DEBUG) << "WakeUpPipe: read failed" << endl;
+#endif
 	}
 }
