4cf02827999bffd913743ffec7cbba260a11b637
Pino Toscano <pino@kde.org>
2009-03-23 15:54:59 +0000

    there are messages in .cc files as well
    svn path=/trunk/extragear/network/ktorrent/; revision=943280

--- Messages.sh
+++ Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 $EXTRACTRC `find . -name \*.ui -o -name \*.rc` >> rc.cpp
-$XGETTEXT `find . -name \*.cpp -o -name \*.h` rc.cpp -o $podir/ktorrent.pot
+$XGETTEXT `find . -name \*.cpp -o -name \*.cc -o -name \*.h` rc.cpp -o $podir/ktorrent.pot
 rm -f rc.cpp
