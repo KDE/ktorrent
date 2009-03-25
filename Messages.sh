a10e6a4ea69e50bb5b0d3707f34714d3896ea8b6
Laurent Montel <montel@kde.org>
2009-03-25 08:10:00 +0000

    Extract text from kcfg
    svn path=/trunk/extragear/network/ktorrent/; revision=944162

--- Messages.sh
+++ Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
-$EXTRACTRC `find . -name \*.ui -o -name \*.rc` >> rc.cpp
+$EXTRACTRC `find . -name \*.ui -o -name \*.rc -o -name \*.kcfg` >> rc.cpp
 $XGETTEXT `find . -name \*.cpp -o -name \*.cc -o -name \*.h` rc.cpp -o $podir/ktorrent.pot
 rm -f rc.cpp
