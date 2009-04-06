dffc87b8a386bb41c3f6d73eb273c6be140f4284
Joris Guisson <joris.guisson@gmail.com>
2009-04-06 07:27:50 +0000

    Stable is now version 3.2.1
    svn path=/branches/stable/extragear-kde4/network/ktorrent/; revision=949884

--- libbtcore/btversion.h
+++ libbtcore/btversion.h
@@ -64,7 +64,7 @@ namespace bt
 	/// Version type of the BTCore library
 	const VersionType VERSION_TYPE = NORMAL;
 	/// Release version number of the BTCore library
-	const Uint32 RELEASE = 0;
+	const Uint32 RELEASE = 1;
 }
 
 #endif
--- libktcore/ktversion.h
+++ libktcore/ktversion.h
@@ -27,11 +27,11 @@ namespace kt
 {
 	const bt::Uint32 MAJOR = 3;
 	const bt::Uint32 MINOR = 2;
-	const bt::Uint32 RELEASE = 0;
+	const bt::Uint32 RELEASE = 1;
 	const bt::VersionType VERSION_TYPE = bt::NORMAL;
-	const char VERSION_STRING[] = "3.2.0";
+	const char VERSION_STRING[] = "3.2.1";
 }
 
-#define KT_VERSION_MACRO "3.2.0"
+#define KT_VERSION_MACRO "3.2.1"
 
 #endif
