c3ab5f1a9101343bddcc1efa757d6017ef633637
Joris Guisson <joris.guisson@gmail.com>
2009-09-22 19:26:35 +0000

    Prepare stable for 3.2.4 tagging
    svn path=/branches/stable/extragear-kde4/network/ktorrent/; revision=1026892

--- libbtcore/btversion.h
+++ libbtcore/btversion.h
@@ -64,7 +64,7 @@ namespace bt
 	/// Version type of the BTCore library
 	const VersionType VERSION_TYPE = NORMAL;
 	/// Release version number of the BTCore library
-	const Uint32 RELEASE = 3;
+	const Uint32 RELEASE = 4;
 }
 
 #endif
