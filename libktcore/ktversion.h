f119e0742162b1c4369c4f433e255b3ce9ffedb0
Joris Guisson <joris.guisson@gmail.com>
2010-05-23 07:00:14 +0000

    Increase version number to 4.0.0
    svn path=/branches/stable/extragear-kde4/network/ktorrent/; revision=1129657

--- libktcore/ktversion.h
+++ libktcore/ktversion.h
@@ -27,11 +27,11 @@ namespace kt
 {
 	const bt::Uint32 MAJOR = 4;
 	const bt::Uint32 MINOR = 0;
-	const bt::Uint32 RELEASE = 1;
-	const bt::VersionType VERSION_TYPE = bt::RELEASE_CANDIDATE;
-	const char VERSION_STRING[] = "4.0rc1";
+	const bt::Uint32 RELEASE = 0;
+	const bt::VersionType VERSION_TYPE = bt::NORMAL;
+	const char VERSION_STRING[] = "4.0.0";
 }
 
-#define KT_VERSION_MACRO "4.0rc1"
+#define KT_VERSION_MACRO "4.0.0"
 
 #endif
