89ff3c90c9f9aafc913eca73e90364015c84313f
Joris Guisson <joris.guisson@gmail.com>
2010-10-16 10:39:39 +0000

    Merge rev 1186422 from stable (fix for 254214)
    svn path=/trunk/extragear/network/ktorrent/; revision=1186423

--- ktorrent/core.cpp
+++ ktorrent/core.cpp
@@ -68,7 +68,7 @@ namespace kt
 {
 	const Uint32 CORE_UPDATE_INTERVAL = 250;
 
-	Core::Core(kt::GUI* gui) : gui(gui),keep_seeding(true),sleep_suppression_cookie(-1)
+	Core::Core(kt::GUI* gui) : gui(gui),keep_seeding(true),sleep_suppression_cookie(-1),exiting(false)
 	{
 		UpdateCurrentTime();
 		qman = new QueueManager();
@@ -898,6 +898,7 @@ namespace kt
 	void Core::onExit()
 	{
 		// stop timer to prevent updates during wait
+		exiting = true;
 		update_timer.stop();
 		
 		net::SocketMonitor::instance().shutdown();
@@ -1032,6 +1033,9 @@ namespace kt
 
 	void Core::update()
 	{
+		if (exiting)
+			return;
+		
 		try
 		{
 			bt::UpdateCurrentTime();
