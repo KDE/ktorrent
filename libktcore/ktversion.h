6bff05315036547665c412359c8d66ebd2947e03
Joris <joris.guisson@gmail.com>
2011-03-14 18:34:03 +0100

    Prepare for 4.1 release

--- libktcore/ktversion.h
+++ libktcore/ktversion.h
@@ -29,8 +29,8 @@ namespace kt
 {
 	const bt::Uint32 MAJOR = 4;
 	const bt::Uint32 MINOR = 1;
-	const bt::Uint32 RELEASE = 1;
-	const bt::VersionType VERSION_TYPE = bt::RELEASE_CANDIDATE;
+	const bt::Uint32 RELEASE = 0;
+	const bt::VersionType VERSION_TYPE = bt::NORMAL;
 	const char VERSION_STRING[] = KT_VERSION_MACRO;
 }
 
