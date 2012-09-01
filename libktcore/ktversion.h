55bae599f7b7825cdb0124dbcb189185f9b7700e
Joris Guisson <joris.guisson@gmail.com>
2012-09-01 10:59:57 +0200

    Prepare for 4.3.0

--- libktcore/ktversion.h
+++ libktcore/ktversion.h
@@ -23,14 +23,14 @@
 #include <version.h>
 #include "util/constants.h"
 
-#define KT_VERSION_MACRO "4.3rc1"
+#define KT_VERSION_MACRO "4.3.0"
 
 namespace kt
 {
 	const bt::Uint32 MAJOR = 4;
 	const bt::Uint32 MINOR = 3;
-	const bt::Uint32 RELEASE = 1;
-	const bt::VersionType VERSION_TYPE = bt::RELEASE_CANDIDATE;
+	const bt::Uint32 RELEASE = 0;
+	const bt::VersionType VERSION_TYPE = bt::NORMAL;
 	const char VERSION_STRING[] = KT_VERSION_MACRO;
 }
 
