aa75958e5925daf89b4e2d2284075033cb1801bb
Joris Guisson <joris.guisson@gmail.com>
2012-10-18 20:01:06 +0200

    Backport to 4.3: Fix crash due to bus error in ip filter plugin
    CCBUG: 307854

---
+++ plugins/ipfilter/ipblocklist.cpp
@@ -0,0 +1,123 @@
+/***************************************************************************
+ *   Copyright (C) 2005 by Joris Guisson                                   *
+ *   joris.guisson@gmail.com                                               *
+ *   ivasic@gmail.com                                                      *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.             *
+ ***************************************************************************/
+#include "ipblocklist.h"
+#include <QFile>
+#include <util/log.h>
+#include <util/constants.h>
+#include <net/address.h>
+
+
+using namespace bt;
+
+namespace kt
+{
+    static Uint32 StringToUint32(const QString& ip)
+    {
+        bool test;
+        Uint32 ret = ip.section('.', 0, 0).toULongLong(&test);
+        ret <<= 8;
+        ret |= ip.section('.', 1, 1).toULong(&test);
+        ret <<= 8;
+        ret |= ip.section('.', 2, 2).toULong(&test);
+        ret <<= 8;
+        ret |= ip.section('.', 3, 3).toULong(&test);
+
+        return ret;
+    }
+
+    IPBlock::IPBlock() : ip1(0), ip2(0)
+    {}
+
+    IPBlock::IPBlock(const IPBlock& block) : ip1(block.ip1), ip2(block.ip2)
+    {}
+
+    IPBlock::IPBlock(const QString& start, const QString& end)
+    {
+        ip1 = StringToUint32(start);
+        ip2 = StringToUint32(end);
+    }
+
+    IPBlockList::IPBlockList()
+    {
+    }
+
+    IPBlockList::~IPBlockList()
+    {
+    }
+
+    bool IPBlockList::blocked(const net::Address& addr) const
+    {
+        if (addr.protocol() == QAbstractSocket::IPv6Protocol || blocks.empty())
+            return false;
+
+        // Binary search the list of blocks which are sorted
+        quint32 ip = addr.toIPv4Address();
+        int begin = 0;
+        int end = blocks.size() - 1;
+        while(true)
+        {
+            if(begin == end)
+                return blocks[begin].constains(ip);
+            else if(begin == end - 1)
+                return blocks[begin].constains(ip) || blocks[end].constains(ip);
+            
+            int pivot = begin + (end - begin) / 2;
+            if(blocks[pivot].constains(ip))
+                return true;
+            else if(ip < blocks[pivot].ip1)
+                end = pivot - 1; // continue in the range [begin, pivot - 1]
+            else // ip > blocks[pivot].ip2
+                begin = pivot + 1; // continue in the range [pivot + 1, end]
+        }
+        return false;
+    }
+
+    bool IPBlockList::load(const QString & path)
+    {
+        QFile file(path);
+        if (!file.open(QIODevice::ReadOnly))
+        {
+            Out(SYS_IPF | LOG_NOTICE) << "Cannot open " << path << ": " << file.errorString() << endl;
+            return false;
+        }
+
+        // Note: the conversion process has sorted the blocks !
+        int num_blocks = file.size() / sizeof(IPBlock);
+        blocks.reserve(num_blocks);
+        while (!file.atEnd() && blocks.size() < num_blocks)
+        {
+            IPBlock block;
+            if (file.read((char*)&block, sizeof(IPBlock)) == sizeof(IPBlock))
+                addBlock(block);
+            else
+                break;
+        }
+
+        Out(SYS_IPF | LOG_NOTICE) << "Loaded " << blocks.size() << " blocked IP ranges" << endl;
+        return true;
+    }
+    
+    void IPBlockList::addBlock(const IPBlock& block)
+    {
+        blocks.append(block);
+    }
+
+}
--- plugins/ipfilter/ipfilterplugin.cpp
+++ plugins/ipfilter/ipfilterplugin.cpp
@@ -25,147 +25,141 @@
 
 #include <interfaces/coreinterface.h>
 #include <interfaces/guiinterface.h>
+#include <interfaces/functions.h>
 #include <util/constants.h>
 #include <util/log.h>
 #include <util/logsystemmanager.h>
 #include <peer/accessmanager.h>
 
-#include <qstring.h>
 
 #include "ipfilterplugin.h"
 #include "ipfilterpluginsettings.h"
-#include "antip2p.h"
+#include "ipblocklist.h"
 
 using namespace bt;
 
-K_EXPORT_COMPONENT_FACTORY(ktipfilterplugin,KGenericFactory<kt::IPFilterPlugin>("ipfilterplugin"))
+K_EXPORT_COMPONENT_FACTORY(ktipfilterplugin, KGenericFactory<kt::IPFilterPlugin>("ipfilterplugin"))
 
 namespace kt
-{	
+{
 
-	IPFilterPlugin::IPFilterPlugin(QObject* parent, const QStringList& args) : Plugin(parent)
-	{
-		Q_UNUSED(args);
-		// setXMLFile("ktpluginui.rc");
-		level1 = 0;
-		connect(&auto_update_timer,SIGNAL(timeout()),this,SLOT(checkAutoUpdate()));
-		auto_update_timer.setSingleShot(true);
-	}
+    IPFilterPlugin::IPFilterPlugin(QObject* parent, const QStringList& args) : Plugin(parent)
+    {
+        Q_UNUSED(args);
+        connect(&auto_update_timer, SIGNAL(timeout()), this, SLOT(checkAutoUpdate()));
+        auto_update_timer.setSingleShot(true);
+    }
 
 
-	IPFilterPlugin::~IPFilterPlugin()
-	{
-	}
+    IPFilterPlugin::~IPFilterPlugin()
+    {
+    }
 
-	void IPFilterPlugin::load()
-	{
-		LogSystemManager::instance().registerSystem(i18n("IP Filter"),SYS_IPF);
-		pref = new IPBlockingPrefPage(this);
-		connect(pref,SIGNAL(updateFinished()),this,SLOT(checkAutoUpdate()));
-		getGUI()->addPrefPage(pref);
-		
-		if (IPBlockingPluginSettings::useLevel1())
-			loadAntiP2P();
-		
-		checkAutoUpdate();
-	}
+    void IPFilterPlugin::load()
+    {
+        LogSystemManager::instance().registerSystem(i18n("IP Filter"), SYS_IPF);
+        pref = new IPBlockingPrefPage(this);
+        connect(pref, SIGNAL(updateFinished()), this, SLOT(checkAutoUpdate()));
+        getGUI()->addPrefPage(pref);
 
-	void IPFilterPlugin::unload()
-	{	
-		LogSystemManager::instance().unregisterSystem(i18n("IP Filter"));
-		getGUI()->removePrefPage(pref);
-		delete pref;
-		pref = 0;
-		if (level1)
-		{
-			AccessManager::instance().removeBlockList(level1);
-			delete level1;
-			level1 = 0;
-		}
-	}
-	
-	bool IPFilterPlugin::loadAntiP2P()
-	{
-		if (level1 != 0)
-			return true;
-		
-		level1 = new AntiP2P();
-		if(!level1->exists())
-		{
-			delete level1;
-			level1 = 0;
-			return false;
-		}
-		level1->loadHeader();
-		AccessManager::instance().addBlockList(level1);
-		return true;
-	}
-	
-	bool IPFilterPlugin::unloadAntiP2P()
-	{
-		if (level1 != 0)
-		{
-			AccessManager::instance().removeBlockList(level1);
-			delete level1;
-			level1 = 0;
-			return true;
-		}
-		else
-			return true;
-	}
-	
-	bool IPFilterPlugin::loadedAndRunning()
-	{
-		return level1 != 0;
-	}
+        if (IPBlockingPluginSettings::useLevel1())
+            loadAntiP2P();
 
-	bool IPFilterPlugin::versionCheck(const QString & version) const
-	{
-		return version == KT_VERSION_MACRO;
-	}
-	
-	void IPFilterPlugin::checkAutoUpdate()
-	{
-		auto_update_timer.stop();
-		if (!loadedAndRunning() || !IPBlockingPluginSettings::autoUpdate())
-			return;
-		
-		KConfigGroup g = KGlobal::config()->group("IPFilterAutoUpdate");
-		bool ok = g.readEntry("last_update_ok",false);
-		QDateTime now = QDateTime::currentDateTime();
-		if (!ok)
-		{
-			QDateTime last_update_attempt = g.readEntry("last_update_attempt",now);
-			// if we cannot do it now, or the last attempt was less then 15 minute ago, try again in 15 minutes
-			if (last_update_attempt.secsTo(now) < AUTO_UPDATE_RETRY_INTERVAL || !pref->doAutoUpdate()) 
-				auto_update_timer.start(AUTO_UPDATE_RETRY_INTERVAL * 1000);
-		}
-		else
-		{
-			QDateTime last_updated = g.readEntry("last_updated",QDateTime());
-			QDateTime next_update;
-			if (last_updated.isNull())
-				next_update = now.addDays(IPBlockingPluginSettings::autoUpdateInterval());
-			else
-				next_update = QDateTime(last_updated).addDays(IPBlockingPluginSettings::autoUpdateInterval());
-				
-			if (now >= next_update)
-			{
-				if (!pref->doAutoUpdate()) // if we cannot do it now, try again in 15 minutes
-					auto_update_timer.start(AUTO_UPDATE_RETRY_INTERVAL * 1000);
-			}
-			else
-			{
-				// schedule an auto update
-				auto_update_timer.start(1000 * (now.secsTo(next_update) + 5));
-				Out(SYS_IPF|LOG_NOTICE) << "Scheduling ipfilter auto update on " << next_update.toString() << endl;
-			}
-		}
-	}
-	
-	void IPFilterPlugin::notification(const QString& msg)
-	{
-		KNotification::event("PluginEvent",msg,QPixmap(),getGUI()->getMainWindow());
-	}
+        checkAutoUpdate();
+    }
+
+    void IPFilterPlugin::unload()
+    {
+        LogSystemManager::instance().unregisterSystem(i18n("IP Filter"));
+        getGUI()->removePrefPage(pref);
+        delete pref;
+        pref = 0;
+        if (ip_filter)
+        {
+            AccessManager::instance().removeBlockList(ip_filter.data());
+            ip_filter.reset();
+        }
+    }
+
+    bool IPFilterPlugin::loadAntiP2P()
+    {
+        if (ip_filter)
+            return true;
+
+        ip_filter.reset(new IPBlockList());
+        if (!ip_filter->load(kt::DataDir() + "level1.dat"))
+        {
+            ip_filter.reset();
+            return false;
+        }
+        AccessManager::instance().addBlockList(ip_filter.data());
+        return true;
+    }
+
+    bool IPFilterPlugin::unloadAntiP2P()
+    {
+        if (ip_filter)
+        {
+            AccessManager::instance().removeBlockList(ip_filter.data());
+            ip_filter.reset();
+            return true;
+        }
+        else
+            return true;
+    }
+
+    bool IPFilterPlugin::loadedAndRunning()
+    {
+        return ip_filter;
+    }
+
+    bool IPFilterPlugin::versionCheck(const QString& version) const
+    {
+        return version == KT_VERSION_MACRO;
+    }
+
+    void IPFilterPlugin::checkAutoUpdate()
+    {
+        auto_update_timer.stop();
+        if (!loadedAndRunning() || !IPBlockingPluginSettings::autoUpdate())
+            return;
+
+        KConfigGroup g = KGlobal::config()->group("IPFilterAutoUpdate");
+        bool ok = g.readEntry("last_update_ok", false);
+        QDateTime now = QDateTime::currentDateTime();
+        if (!ok)
+        {
+            QDateTime last_update_attempt = g.readEntry("last_update_attempt", now);
+            // if we cannot do it now, or the last attempt was less then 15 minute ago, try again in 15 minutes
+            if (last_update_attempt.secsTo(now) < AUTO_UPDATE_RETRY_INTERVAL || !pref->doAutoUpdate())
+                auto_update_timer.start(AUTO_UPDATE_RETRY_INTERVAL * 1000);
+        }
+        else
+        {
+            QDateTime last_updated = g.readEntry("last_updated", QDateTime());
+            QDateTime next_update;
+            if (last_updated.isNull())
+                next_update = now.addDays(IPBlockingPluginSettings::autoUpdateInterval());
+            else
+                next_update = QDateTime(last_updated).addDays(IPBlockingPluginSettings::autoUpdateInterval());
+
+            if (now >= next_update)
+            {
+                if (!pref->doAutoUpdate()) // if we cannot do it now, try again in 15 minutes
+                    auto_update_timer.start(AUTO_UPDATE_RETRY_INTERVAL * 1000);
+            }
+            else
+            {
+                // schedule an auto update
+                auto_update_timer.start(1000 * (now.secsTo(next_update) + 5));
+                Out(SYS_IPF | LOG_NOTICE) << "Scheduling ipfilter auto update on " << next_update.toString() << endl;
+            }
+        }
+    }
+
+    void IPFilterPlugin::notification(const QString& msg)
+    {
+        KNotification::event("PluginEvent", msg, QPixmap(), getGUI()->getMainWindow());
+    }
 
 }
