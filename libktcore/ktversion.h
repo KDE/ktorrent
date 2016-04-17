8ffd07667a0905da04c3e3ec889e732e3c7a5de0
Nick Shaforostoff <shafff@ukr.net>
2016-04-17 18:08:34 +0300

    update version

--- libktcore/ktversion.h
+++ libktcore/ktversion.h
@@ -23,13 +23,13 @@
 #include <version.h>
 #include "util/constants.h"
 
-#define KT_VERSION_MACRO "5.0"
+#define KT_VERSION_MACRO "5.0.1"
 
 namespace kt
 {
     const bt::Uint32 MAJOR = 5;
     const bt::Uint32 MINOR = 0;
-    const bt::Uint32 RELEASE = 0;
+    const bt::Uint32 RELEASE = 1;
     const bt::VersionType VERSION_TYPE = bt::DEVEL;
     const char VERSION_STRING[] = KT_VERSION_MACRO;
 }
