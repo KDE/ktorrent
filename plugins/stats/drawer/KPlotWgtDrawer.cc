61add001fc3230f6413470d382693058980faf10
Jack Hill <jackhill3103@gmail.com>
2023-10-30 23:10:50 +0000

    Return null QUuid instead of nullptr

--- plugins/stats/drawer/KPlotWgtDrawer.cc
+++ plugins/stats/drawer/KPlotWgtDrawer.cc
@@ -322,7 +322,7 @@ QString KPlotWgtDrawer::makeLegendString()
 QUuid KPlotWgtDrawer::getUuid(const size_t idx) const
 {
     if (idx >= static_cast<size_t>(plotObjects().size())) {
-        return nullptr;
+        return QUuid{};
     }
 
     return pmUuids.at(idx);
