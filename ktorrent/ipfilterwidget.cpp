a0f835db257b9de5bb5fcbd014839e40de50b33a
Eastern Li <EasternLi@outlook.com>
2024-03-30 19:48:55 +0800

    Avoid displaying "(I18N_ARGUMENT_MISSING)"

--- ktorrent/ipfilterwidget.cpp
+++ ktorrent/ipfilterwidget.cpp
@@ -89,8 +89,8 @@ void IPFilterWidget::add()
         if (!regex_match(ip.toStdString(), rx) || !filter_list->add(ip)) {
             KMessageBox::error(this,
                                i18n("Invalid IP address <b>%1</b>. IP addresses must be in the format 'XXX.XXX.XXX.XXX'."
-                                    "<br/><br/>You can also use wildcards like '127.0.0.*' or specify ranges like '200.10.10.0-200.10.10.40'.")
-                                   .arg(ip));
+                                    "<br/><br/>You can also use wildcards like '127.0.0.*' or specify ranges like '200.10.10.0-200.10.10.40'.",
+                                    ip));
 
             return;
         }
